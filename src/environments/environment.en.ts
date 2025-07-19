4e3cb4f5c5cc0fb8db1328ae04dbbc12240b73d2
juanigna <juignavazquez@gmail.com>
2025-07-19 17:47:11 -0300

    configuracion regionalizacion

--- src/environments/environment.en.ts
+++ src/environments/environment.en.ts
@@ -2,5 +2,7 @@ export const environment = {
     production: false,
     apiUrl: "http://localhost:8084/api/v1/indec/", // O ajusta según tus necesidades
     language: 'en', // Aquí puedes agregar configuraciones específicas para español
+    user: "indec123",
+    password: "indec123"
   };
   
--- src/environments/environment.es-AR.ts
+++ src/environments/environment.es-AR.ts
@@ -2,4 +2,6 @@ export const environment = {
     production: false,
     apiUrl: "http://localhost:8084/api/v1/indec/",
     language: 'es-AR',
+    user: "indec123",
+    password: "indec123"
 };
